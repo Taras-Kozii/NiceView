d61afd4e37b5239e3f8ac8bc0fb40797ad87873c
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-05-06 12:56:29 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const slider = new Swiper(".slider-gallery", {
+  navigation: {
+    nextEl: ".slider-gallery__next",
+    prevEl: ".slider-gallery__prev" 
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    dynamicBullets: true,
+	},
+
+  autoHeight: true,
+  on: {
+    slideChangeTransitionEnd: function () {
+      this.updateAutoHeight();
+    },
+  },
+  speed: 800,
+  initialSlide: 2,
+  centeredSlides: true,
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true 
+  },
+  breakpoints: { 
+    1024: { slidesPerView: 3 }
+  },
+});
